974b91ac5d6d4639ecec3f7e5a624715987bc8fd
Mohammad Hussain <khatreemohd@gmail.com>
2014-04-03 23:10:16 +0530

    sample json for app

---
+++ example1/app/data/data.js
@@ -0,0 +1,26 @@
+var data = {
+  "product1": {
+    "Price": 100,
+    "img": "/img/cake1.jpg",
+    "desc": "Product Description. images are not for commercial use",
+    "title": "Product 1"
+  },
+  "product2": {
+    "Price": 100,
+    "img": "/img/cake2.jpg",
+    "desc": "Product Description. images are not for commercial use",
+    "title": "Product 2"
+  },
+  "product3": {
+    "Price": 200,
+    "img": "/img/cake3.jpg",
+    "desc": "Product Description. images are not for commercial use",
+    "title": "Product 3"
+  },
+  "product4": {
+    "Price": 250,
+    "img": "/img/cake4.jpg",
+    "desc": "Product Description. images are not for commercial use",
+    "title": "Product 4"
+  }
+}
